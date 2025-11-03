c1cb72919b61dd4f7586aed7cdf3aeae383ede69
Aanshu3 <raghavanshu1204@gmail.com>
2025-11-03 12:42:29 +0530

    Fix vercel deployment config

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:5000'
-    }
+      '/api': {
+        target: 'http://localhost:5000', // local backend only
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   build: {
     outDir: 'dist'
